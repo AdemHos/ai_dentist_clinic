776f5ca7f6cb87fa48663763d31712ff61519120
AdemHos <ademhos1907@outlook.com>
2026-08-09 22:58:50 +0300

    First Commit

---
+++ src/app/page.tsx
@@ -0,0 +1,14 @@
+'use client'
+
+import { SignUpButton } from "@clerk/nextjs";
+
+
+
+
+export default function Home() {
+  return (
+    <div>
+       <SignUpButton>Sign Up</SignUpButton>
+    </div>
+  );
+}
